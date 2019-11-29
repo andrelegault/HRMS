2fe1604f8b0b5815f9797997c6dd40764b3bb9ae
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2019-11-29 03:46:07 -0500

    Add EmployeeServlet

---
+++ src/main/Employee.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Employee extends User {
+	private String role;
+	
+	public Employee(String first, String last, String role) {
+		super(first, last);
+		this.role = role;
+	}
+	
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public String getRole() {
+		return this.role;
+	}
+
+}
--- src/main/User.java
+++ src/main/User.java
@@ -1,5 +1,32 @@
 package main;
 
 public class User {
-
+	private String first;
+	private String last;
+	private int id;
+	
+	public User(String first, String last) {
+		this.setFirst(first);
+		this.setLast(last);
+	}
+	
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
+	public String getFirst() {
+		return this.first;
+	}
+	
+	public String getLast() {
+		return this.last;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
 }
