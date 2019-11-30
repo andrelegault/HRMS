2deea0708e2eaa83636d2f72fd6544e196886200
nickode <nicolasnsamaha@hotmail.com>
2019-11-30 15:31:28 -0500

    changed project file structure up

--- src/main/User.java
+++ src/main/User.java
@@ -1,32 +1,50 @@
 package main;
 
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class User {
-	private String first;
-	private String last;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
-	public User(String first, String last) {
-		this.setFirst(first);
-		this.setLast(last);
+	@Column
+	private String username;
+	@Column
+	private String password;
+	@Column
+	private String email;
+	
+	public int getId()
+	{
+		return id;
 	}
 	
-	public void setFirst(String first) {
-		this.first = first;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	
-	public void setLast(String last) {
-		this.last = last;
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
-	public String getFirst() {
-		return this.first;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
-	public String getLast() {
-		return this.last;
-	}
-	
-	public int getId() {
-		return this.id;
-	}
 }
