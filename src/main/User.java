50dec71f4b102514f3129cf048cf5cbd148b0319
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2019-12-01 20:24:44 -0500

    Remove unused functions

--- src/main/User.java
+++ src/main/User.java
@@ -1,50 +1,32 @@
 package main;
 
-import javax.persistence.Entity;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String first;
+	private String last;
 	private int id;
 	
-	@Column
-	private String username;
-	@Column
-	private String password;
-	@Column
-	private String email;
-	
-	public int getId()
-	{
-		return id;
+	public User(String first, String last) {
+		this.setFirst(first);
+		this.setLast(last);
 	}
 	
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
+	public void setFirst(String first) {
+		this.first = first;
 	}
 	
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
+	public void setLast(String last) {
+		this.last = last;
 	}
 	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
+	public String getFirst() {
+		return this.first;
 	}
 	
+	public String getLast() {
+		return this.last;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
 }
